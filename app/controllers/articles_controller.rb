2d6cd4e6bec9fe4938db31c72846f44a9827606c
BriSkii <bwhit034@outlook.com>
2017-06-19 00:40:05 -0400

    Added the edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,14 +19,21 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
-    end  
-
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def destroy
