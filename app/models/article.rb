eb017e4e51f68703331e71196dfadae004a93402
BriSkii <bwhit034@outlook.com>
2017-06-20 22:05:34 -0400

    Added categories to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,6 @@ class Article < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619233518) do
+ActiveRecord::Schema.define(version: 20170621012306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,14 @@ ActiveRecord::Schema.define(version: 20170619233518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "category_id"
     t.index ["slug"], name: "index_articles_on_slug", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
